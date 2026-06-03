9c68fe56699d61d32c9f91fa7139f18cc566efb8
eastmoe <eastmoe@github.com>
2026-06-03 13:25:20 +0800

    修复启动comfyui时出现的错误提示[Comfy-MSST] Failed to import nodes: No module named 'msst_webui'

--- __init__.py
+++ __init__.py
@@ -1,3 +1,11 @@
+import sys
+from pathlib import Path
+
+PLUGIN_DIR = Path(__file__).resolve().parent
+plugin_dir_str = str(PLUGIN_DIR)
+if plugin_dir_str not in sys.path:
+    sys.path.insert(0, plugin_dir_str)
+
 try:
     from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 except Exception as exc:
